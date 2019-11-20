a3c246fc4c392b8a54c8ea420e06444cf6b3dd85
Rupa <rupa211984@gmail.com>
2019-11-19 21:40:01 -0800

    Done

--- index.js
+++ index.js
@@ -5,3 +5,46 @@ const questions = [
 ]
 
 let question;
+
+function appendQuestion(question) {
+  const qDiv = document.querySelector('.question-container');
+  qDiv.innerHTML = question.questionText;
+}
+
+function askQuestionThen(time) {
+  question = questions[0];
+  appendQuestion(question);
+
+  return new Promise(resolve => {
+    setTimeout(resolve, time);
+  });
+}
+
+function removeQuestion() {
+  const qDiv = document.querySelector('.question-container');
+  qDiv.innerHTML = '';
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  return askQuestionThen(time)
+    .then(() => { removeQuestion() });
+}
+
+function trueAndFalseButtons() {
+  return [document.querySelector('.btn.green'), document.querySelector('.btn.red')];
+}
+
+function toggleTrueAndFalseButtons() {
+  trueAndFalseButtons().forEach(btn => { 
+    btn.classList.contains('hide') ? btn.classList.remove('hide') : btn.classList.add('hide');
+  });
+}
+
+function displayQuestionOnClick() {
+  const askBtn = document.querySelector('.waves-effect');
+
+  askBtn.addEventListener('click', () => {
+    toggleTrueAndFalseButtons();
+    askQuestionThenRemoveQuestion(10000);
+  });
+}
